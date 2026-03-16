7ffba4ecec8069a5f42972446b5a80fc695aba21
Eddie <eddiehlin@utexas.edu>
2026-03-15 22:44:26 -0500

    Fixed test errors so now non digit and improperly formatted numbers are tested properly

--- my_code.py
+++ my_code.py
@@ -1,6 +1,8 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
+  if not phone_num_to_fix.isdigit() or len(phone_num_to_fix) != 10:
+     raise ValueError("Invalid phone number format (not numbers or more than 10 numbers)")
   # given "5125558823". Split the parts, then recombine and return
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
